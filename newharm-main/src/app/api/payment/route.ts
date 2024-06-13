1f5080c09dc0f0020be0864e503f389a6727d841
Tiago Becker <beckertiago09@gmail.com>
2024-06-13 12:46:05 -0300

    teste

--- newharm-main/src/app/api/payment/route.ts
+++ newharm-main/src/app/api/payment/route.ts
@@ -59,7 +59,7 @@ export async function POST(req: Request, res: Response) {
               expires_in: 108000,
               default_payment_method: "pix",
               accepted_payment_methods: ["pix", "credit_card"],
-              success_url: "http://locahost:3000/sucess",
+              success_url: "https://guedespublica-oes-xfyd.vercel.app/sucess",
               skip_checkout_success_page: false,
               customer_editable: true,
               billing_address_editable: true,
@@ -97,8 +97,9 @@ export async function POST(req: Request, res: Response) {
           },
         ],
         
-        
-        closed: true,
+        customer_editable: true,
+         billing_address_editable: true,
+         closed: true,
       }),
      
     }
